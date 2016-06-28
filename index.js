3f1d9457acf14ae19d3cdf7992e902edcc6e14aa
rich-farr <scarletmanuka@gmail.com>
2016-06-28 13:54:02 +1200

    Capitalise ISS and remove decimals from speed and altitude

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var iss = require('./views/iss.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
@@ -9,9 +10,25 @@ xhr.get(endpoint, function (err, data) {
   }
 
   // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+  console.log("CLANG", data.body) // FYI: data.body is a string
+  var obj = JSON.parse(data.body)
+  var fixData = function (object) {
+  	object.name = object.name.toUpperCase()
+  	object.altitude = parseInt(object.altitude.toFixed(0))
+  	object.velocity = parseInt(object.velocity.toFixed(0))
+  	return object
+  }
+
+  var test = fixData(obj)
+
+  //console.log("AUGH!", typeof test)
+
+  //var anObj = JSON.parse(data.body)
 
   // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+  var target = document.getElementById('heading')
+  target.innerHTML = greeting({name: ', person that is interested in science!'})
+
+  var position = document.getElementById('content')
+  position.innerHTML = iss(test)
 })
