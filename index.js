83d2bad58c2d75ab9af24289270d6c5f64e40ccb
rich-farr <scarletmanuka@gmail.com>
2016-06-28 16:31:19 +1200

    Updates

--- index.js
+++ index.js
@@ -4,38 +4,40 @@ var iss = require('./views/iss.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-var run = xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
+window.onload = doTheThing()
 
-  // In case you're curious
-  //console.log("CLANG", data.body) // FYI: data.body is a string
-  var obj = JSON.parse(data.body)
-  var fixData = function (object) {
-  	object.name = object.name.toUpperCase()
-  	object.altitude = parseInt(object.altitude.toFixed(0))
-  	object.velocity = parseInt(object.velocity.toFixed(0))
-  	object.apikey = 'AIzaSyB9P4kGA3lSVTem-QY57vKuJmIO3iczpT4'
-  	return object
-  }
+function doTheThing() {
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
 
-  console.log("BOING", fixData(obj))
-  var newObj = fixData(obj)
+    // In case you're curious
+    //console.log("CLANG", data.body) // FYI: data.body is a string
+    var obj = JSON.parse(data.body)
+    var fixData = function (object) {
+    	object.name = object.name.toUpperCase()
+    	object.altitude = parseInt(object.altitude.toFixed(0))
+    	object.velocity = parseInt(object.velocity.toFixed(0))
+    	object.apikey = 'AIzaSyB9P4kGA3lSVTem-QY57vKuJmIO3iczpT4'
+    	return object
+    }
 
-  // Replace 'Space' below with the response
-  var target = document.getElementById('heading')
-  target.innerHTML = greeting({name: ', person that is interested in science!'})
+    console.log("BOING", fixData(obj))
+    var newObj = fixData(obj)
 
-  var position = document.getElementById('content')
-  position.innerHTML = iss(newObj)
+    // Replace 'Space' below with the response
+    var target = document.getElementById('heading')
+    target.innerHTML = greeting({name: ', person that is interested in science!'})
 
-})
+    var position = document.getElementById('content')
+    position.innerHTML = iss(newObj)
+  })
+}
 
 document.getElementById("btn").addEventListener("click", function(){
     console.log("This worked, hooray!")
-    console.log(run)
-    return run
+    doTheThing()
 });
 
 
